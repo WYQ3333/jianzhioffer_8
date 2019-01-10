5f6b77f225d5d83ed33eb2754f4d1c2a3d3751ed
浅忆梦微凉 <3538922590@qq.com>
2019-01-10 23:23:29 +0800

    解决错误

--- 剑指offer_8_第一次只出现一次的字符/test.cpp
+++ 剑指offer_8_第一次只出现一次的字符/test.cpp
@@ -9,21 +9,23 @@ public:
 	int FirstNotRepeatingChar(string str) {
 		int array[26] = { 0 };
 		int i = 0;
+		int temp = 0;
 		for (i = 0; i < str.size(); ++i){
-			int temp = (str[i] - 'a');
-			if (array[temp]>1){
-				continue;
+			temp = str[i] - 'a';
+			if (array[temp] <= 1){
+				++array[temp];
 			}
-			++array[temp];
 		}
-		char c;
+		int minpos = 0;
 		for (i = 0; i < 26; ++i){
 			if (array[i] == 1){
-				c = array[i]+'a';
+				int temp = str.find('a' + array[i], 0);
+			}
+			if (temp < minpos){
+				minpos = temp;
 			}
 		}
-		int result = str.find(c, 0);
-		return result;
+		return minpos;
 	}
 };
 
